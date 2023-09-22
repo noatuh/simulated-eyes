8091308e9da853fe3f2144fd29108058619c82a7
noatuh <noatuh@outlook.com>
2023-09-22 05:06:34 -0400

    feature branch

--- app.py
+++ app.py
@@ -5,90 +5,101 @@ import random
 # Initialize pygame
 pygame.init()
 
-# Get screen dimensions
-screen_info = pygame.display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-# Create a full-screen window with no border
+# Get the screen resolution
+screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h
+
+# Create a borderless, fullscreen window
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN | pygame.NOFRAME)
 
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# Eye properties
+eye_width = screen_width // 4
+eye_height = screen_height // 2
+eye_gap = screen_width // 8
+pupil_radius = eye_width // 5
 
-# Eye parameters
-eye_width = screen_width // 10  # Doubled the width
-eye_height = screen_width // 5  # Doubled the height
-eye_x_spacing = screen_width // 4
-left_eye_x = (screen_width // 2) - eye_x_spacing
-right_eye_x = (screen_width // 2) + eye_x_spacing
-eye_y = screen_height // 2
-eye_move_speed = screen_width // 300
-eye_max_offset = screen_width // 30
+# Pupil initial positions and target positions
+left_pupil_position = [screen_width // 4, screen_height // 2]
+right_pupil_position = [3 * screen_width // 4, screen_height // 2]
+left_pupil_target = [screen_width // 4, screen_height // 2]
+right_pupil_target = [3 * screen_width // 4, screen_height // 2]
 
-# Behavior states
-blink_time = random.randint(2000, 5000)
-blink_duration = 300
-blink_timer = 0
-eyes_open = True
-move_timer = 0
-move_time = random.randint(2000, 7000)
-move_direction = random.choice([-1, 1])
-eye_offset = 0
-eye_position = 'center'  # Can be 'left', 'center', or 'right'
+# Movement speed for pupils
+pupil_speed = 5
+
+# Clock for frame rate control
+clock = pygame.time.Clock()
 
 def draw_eyes():
-    pygame.draw.ellipse(screen, WHITE, (left_eye_x + eye_offset - eye_width//2, eye_y - eye_height//2, eye_width, eye_height))
-    pygame.draw.ellipse(screen, WHITE, (right_eye_x + eye_offset - eye_width//2, eye_y - eye_height//2, eye_width, eye_height))
+    pygame.draw.ellipse(screen, WHITE, (screen_width // 4 - eye_width // 2, screen_height // 2 - eye_height // 2, eye_width, eye_height))
+    pygame.draw.ellipse(screen, WHITE, (3 * screen_width // 4 - eye_width // 2, screen_height // 2 - eye_height // 2, eye_width, eye_height))
+    pygame.draw.circle(screen, BLACK, left_pupil_position, pupil_radius)
+    pygame.draw.circle(screen, BLACK, right_pupil_position, pupil_radius)
 
-clock = pygame.time.Clock()
+def set_new_target_synced():
+    boundary = pupil_radius + 20
+    x_offset = random.randint(-eye_width // 2 + boundary, eye_width // 2 - boundary)
+    y_offset = random.randint(-eye_height // 2 + boundary, eye_height // 2 - boundary)
+    left_pupil_target[0] = screen_width // 4 + x_offset
+    left_pupil_target[1] = screen_height // 2 + y_offset
+    right_pupil_target[0] = 3 * screen_width // 4 + x_offset
+    right_pupil_target[1] = screen_height // 2 + y_offset
+
+def move_pupils_smoothly_synced():
+    if abs(left_pupil_position[0] - left_pupil_target[0]) > pupil_speed:
+        if left_pupil_position[0] < left_pupil_target[0]:
+            left_pupil_position[0] += pupil_speed
+            right_pupil_position[0] += pupil_speed
+        else:
+            left_pupil_position[0] -= pupil_speed
+            right_pupil_position[0] -= pupil_speed
+
+    if abs(left_pupil_position[1] - left_pupil_target[1]) > pupil_speed:
+        if left_pupil_position[1] < left_pupil_target[1]:
+            left_pupil_position[1] += pupil_speed
+            right_pupil_position[1] += pupil_speed
+        else:
+            left_pupil_position[1] -= pupil_speed
+            right_pupil_position[1] -= pupil_speed
+
+blink_timer = 0
+movement_timer = 0
+blink_duration = 20
+movement_duration = 60
+
+set_new_target_synced()
+
 while True:
-    dt = clock.tick(60)
-    
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # Check for the "q" key press
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 pygame.quit()
                 sys.exit()
-
-    # Blinking logic
-    blink_timer += dt
-    if blink_timer >= blink_time:
-        eyes_open = not eyes_open
-        if eyes_open:
-            blink_time = random.randint(2000, 5000)
-        else:
-            blink_time = blink_duration
-        blink_timer = 0
-
-    # Eye movement logic
-    move_timer += dt
-    if move_timer >= move_time:
-        if eye_position == 'center':
-            move_direction = random.choice([-1, 1])
-        elif eye_position == 'left':
-            move_direction = 1
-        else:
-            move_direction = -1
-        
-        move_time = random.randint(1000, 3000)
-        move_timer = 0
-
-    if -eye_max_offset <= eye_offset + move_direction * eye_move_speed <= eye_max_offset:
-        eye_offset += move_direction * eye_move_speed
-        if eye_offset == eye_max_offset:
-            eye_position = 'right'
-        elif eye_offset == -eye_max_offset:
-            eye_position = 'left'
-        else:
-            eye_position = 'center'
-
-    screen.fill(BLACK)
-    if eyes_open:
+    
+    blink_timer += 1
+    movement_timer += 1
+    
+    if blink_timer > 100 and blink_timer < (100 + blink_duration):
+        pass
+    else:
         draw_eyes()
+    
+    if movement_timer > movement_duration:
+        set_new_target_synced()
+        movement_timer = 0
+    
+    move_pupils_smoothly_synced()
+    
+    if blink_timer > 200:
+        blink_timer = 0
+    
     pygame.display.flip()
- 
+    clock.tick(60)
